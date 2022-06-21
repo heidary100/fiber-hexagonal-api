8e079c1ec298526a80efc96426a80d4b3f524383
Mohammad <heidary100@gmail.com>
2022-06-22 01:10:49 +0430

    Dockerized go app, added docker-compose for running go and mongo

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -40,7 +40,7 @@ func main() {
 func databaseConnection() (*mongo.Database, context.CancelFunc, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb://username:password@localhost:27017/fiber").SetServerSelectionTimeout(5*time.
+		"mongodb://root:rootpassword@localhost:27017").SetServerSelectionTimeout(5*time.
 		Second))
 	if err != nil {
 		cancel()
